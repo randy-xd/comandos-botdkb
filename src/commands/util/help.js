30748ab4ebabf173ba3b6a105d460fab8501b170
randy-xd <71105026+randy-xd@users.noreply.github.com>
2020-11-13 12:03:22 -0400

    Update help.js

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -55,20 +55,20 @@ module.exports = class HelpCommand extends Command {
 				const messages = [];
 				try {
 					messages.push(await msg.direct(help));
-					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Te envió un DM con información..'));
 				} catch(err) {
-					messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+					messages.push(await msg.reply('No puedo enviarle el DM de ayuda.  Probablemente tengas DM desactivados.'));
 				}
 				return messages;
 			} else if(commands.length > 15) {
-				return msg.reply('Multiple commands found. Please be more specific.');
+				return msg.reply('Se encontraron varios comandos.  Por favor sé más específico.');
 			} else if(commands.length > 1) {
 				return msg.reply(disambiguation(commands, 'commands'));
 			} else {
 				return msg.reply(
-					`Unable to identify command. Use ${msg.usage(
+					`No se puede identificar el comando.  Utilizar${msg.usage(
 						null, msg.channel.type === 'dm' ? null : undefined, msg.channel.type === 'dm' ? null : undefined
-					)} to view the list of all commands.`
+					)} para ver la lista de todos los comandos.`
 				);
 			}
 		} else {
@@ -76,7 +76,7 @@ module.exports = class HelpCommand extends Command {
 			try {
 				messages.push(await msg.direct(stripIndents`
 					${oneLine`
-						To run a command in ${msg.guild ? msg.guild.name : 'any server'},
+						Para ejecutar un comando en ${msg.guild ? msg.guild.name : 'cualquier servidor'},
 						use ${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
 						For example, ${Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
 					`}
